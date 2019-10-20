106732c174b8c82bfb171df7fbb87e457b800f8f
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 14:03:39 +0700

    Solution-Stack

---
+++ 4_6_Solution-Stack/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_6_Solution_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
